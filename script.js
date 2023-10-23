adf1676055f5b3e941a31229806fa85bdb2fda81
Atud Jerry <atudjerry33@gmail.com>
2023-10-23 04:57:12 +0100

    store items setup

--- script.js
+++ script.js
@@ -12,31 +12,31 @@ const func = async()=>{
     products(data)
     const featured = store.filter( (product)=>{
         product.featured ===true
-        // if(product.featured){
-        //     const parent= document.querySelector('.featured')
-        //     parent.innerHTML =   `  <div class="cards">
-        //     <div class="card">
-        //       <img class="featuredImages" src=${product.image}>
-        //       <p class="productName">nice fruit</p>
-        //       <p class="price">$87</p>
+        if(product.featured){
+            const parent= document.querySelector('.featured')
+            parent.innerHTML =   `  <div class="cards">
+            <div class="card">
+              <img class="featuredImages" src=${product.image}>
+              <p class="productName">nice fruit</p>
+              <p class="price">$87</p>
             
       
-        //       <div class="productIcons">
-        //         <div class="background1">
-        //           <p class="pIcon"> <i class="fa fa-search "></i></p>
+              <div class="productIcons">
+                <div class="background1">
+                  <p class="pIcon"> <i class="fa fa-search "></i></p>
           
-        //         </div>
+                </div>
           
-        //         <div class="background2">
-        //           <p class="pIcon"> <i class="fa fa-shopping-cart"></i></p>
+                <div class="background2">
+                  <p class="pIcon"> <i class="fa fa-shopping-cart"></i></p>
           
-        //         </div>
+                </div>
           
-        //       </div>
-        //     </div>`
+              </div>
+            </div>`
           
 
-        // } 
+        } 
         
         
     })
